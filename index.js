58af8e3430e2559570b001b633a0a5fdd09b9f25
Andres Jimenez <Andres.F.Jimenez.F@gmail.com>
2020-01-01 21:13:17 -0800

    added more styling and text into the javascript file

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var time = moment().format("h:mm:ss a")
-
-if( parseInt(time) < 12){
-    $("body").css("background-image: url('https://media3.giphy.com/media/DL6Ji9CMrtlok/giphy.gif?cid=790b761107dbd61c95f6196b303aa3f737bd00f7a3586e69&rid=giphy.gif')")
+if( parseInt(time) > 12){
+    $("body").css({"background-image":"url('https://media3.giphy.com/media/DL6Ji9CMrtlok/giphy.gif?cid=790b761107dbd61c95f6196b303aa3f737bd00f7a3586e69&rid=giphy.gif')"})
 }
+
+function animate(){
+    $("#folder").html("&#xf07b");
+}
+animate()
+$("svg").on("click",function(){
+    $("#folder").empty() 
+    $("#folder").html("&#xf07c")
+
+    console.log("hi")
+})
